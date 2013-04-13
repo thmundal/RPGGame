52247b0c0dc19726831e9d3cd4a3ed84f9381191
Thomas Mundal <thmundal@gmail.com>
2013-04-13 22:03:33 +0200

    Added code for testing mouseplacement

--- Assets/Scripts/GUI/GUI_root.cs
+++ Assets/Scripts/GUI/GUI_root.cs
@@ -2,13 +2,34 @@ using UnityEngine;
 using System.Collections;
 
 public class GUI_root : MonoBehaviour {
-	GUI_Element test;
+	private GUI_Element test;
+	private bool _isMousePlaceInProgress;
+	
 	void Start() {
 		test = new GUI_Element(GUI_Type.Button, new Vector2(100, 1), new Vector2(100, 100));
+		_isMousePlaceInProgress = false;
 	}
 	
 	void OnGUI() {
-		if(test.OnClick())
-			Debug.Log ("blah");
+		if(test.OnClick()) {
+			if(!_isMousePlaceInProgress) {
+				GameObject _objectToPlace = Instantiate(Resources.LoadAssetAtPath ("Assets/Prefabs/enemyAITest.prefab", typeof(GameObject))) as GameObject;
+				_objectToPlace.GetComponent<BoxCollider>().enabled = false;
+				
+				Messenger<GameObject>.Broadcast("placeObject", _objectToPlace);
+			}
+		}
+	}
+	
+	public void OnEnable() {
+		Messenger<bool>.AddListener("mousePlaceRegister", mousePlaceRegister);
+	}
+	
+	public void OnDisable() {
+		Messenger<bool>.RemoveListener("mousePlaceRegister", mousePlaceRegister);
+	}
+	
+	private void mousePlaceRegister(bool yesNo) {
+		_isMousePlaceInProgress = yesNo;
 	}
 }
